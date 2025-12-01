db734ab932f7592916eccaa7b76c10a6ca1cf187
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-01 14:44:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+  const radius = parseFloat(document.getElementById("radius").value);
+
+    const volumeField = document.getElementById("volume");
+
+    if (isNaN(radius) || radius < 0) {
+        volumeField.value = "NaN";
+        return false;
+    }
+
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volumeField.value = volume.toFixed(4);
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
